b7381a7cce26a6a0465b210bddfef9c57fcd857b
JayvonK <jaykhuth@gmail.com>
2024-03-13 08:43:13 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBarComponent from './Components/NavBarComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBarComponent />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={''}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
