b8a3f3f30e22c874c9debd164c617e784ccfc2c0
JayvonK <jaykhuth@gmail.com>
2024-03-14 07:10:46 -0700

    info component

--- src/Components/InfoComponent.tsx
+++ src/Components/InfoComponent.tsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
 
-const InfoComponent = () => {
+const InfoComponent = (props: {
+  jobLink: string;
+  job: string;
+  hq: string;
+  why: string;
+  benefits: string[];
+  requirements: string[];
+}) => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <Container>
+        <div className="box">
+          <h2 className="angkor boxTxt  noPad">
+            <a target="_blank" className="textDark" href={props.jobLink}>
+              {props.job}
+            </a>
+          </h2>
+        </div>
 
-export default InfoComponent
+        <Row>
+          <Col>
+            <img className="sideBox imgBorder" src={props.hq} />
+          </Col>
+          <Col>
+            <div className="whyBox">
+              <h2 className="angkor boxTxt textDark noPad">WHY</h2>
+            </div>
+            <p className="text-blu whyTxt">{props.why}</p>
+          </Col>
+        </Row>
+
+        <div className="benefitsBox">
+          <h2 className="angkor boxTxt textDark noPad">Benefits</h2>
+        </div>
+
+        <Row>
+          {props.benefits.map((txt, i) => (
+            <Col key={"benefit" + i} sm={4}>
+              <ul>
+                <li className="text-blu whyTxt">{txt}</li>
+              </ul>
+            </Col>
+          ))}
+        </Row>
+
+        <div className="benefitsBox">
+          <h2 className="angkor boxTxt textDark noPad">Requirements</h2>
+        </div>
+
+        <Row>
+          {props.requirements.map((txt, i) => (
+            <Col key={"requirement" + i} sm={4}>
+              <ul>
+                <li className="text-blu whyTxt">{txt}</li>
+              </ul>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default InfoComponent;
